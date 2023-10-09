0b6153ae4d6f4e0eb20578bdca3bee248297259f
Eduarda dos Santos de Oliveira <106679320+oliveira-eduarda@users.noreply.github.com>
2023-10-09 11:39:11 -0300

    aula 09/10

---
+++ reconhecimentoDeVoz.js
@@ -0,0 +1,11 @@
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak )
+
+function onSpeak(e){
+    console.log(e.results[0][0].transcipt)
+}
